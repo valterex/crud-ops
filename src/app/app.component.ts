1ad4c42978d74af7fe193166bde27b8723a7ad99
Valter <vcvijic@gmail.com>
2020-11-02 19:45:55 +0100

    minor root changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-crud-fake-api';
+  title = 'Angular CRUD with JSON placeholder fake API';
 }
