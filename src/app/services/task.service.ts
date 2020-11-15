6f680ba3a848da42d1e9cb9909bd448617653884
Valter <vcvijic@gmail.com>
2020-11-15 21:46:59 +0100

    Implement task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
-
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 import { Task } from '../interfaces/task';
 
 @Injectable({
@@ -11,9 +10,55 @@ import { Task } from '../interfaces/task';
 export class TaskService {
   private TASKS_URL = 'https://jsonplaceholder.typicode.com/todos';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.TASKS_URL);
+    return this.http.get<Task[]>(this.TASKS_URL).pipe(
+      tap((_) => console.log('fetched tasks')),
+      catchError(this.handleError<Task[]>([]))
+    );
+  }
+
+  getTask(id: number): Observable<Task> {
+    const url = `${this.TASKS_URL}/${id}`;
+    return this.http.get<Task>(url).pipe(catchError(this.handleError<Task>()));
+  }
+
+  updateTask(task: Task): Observable<any> {
+    return this.http
+      .put(this.TASKS_URL, task, this.httpOptions)
+      .pipe(catchError(this.handleError<any>('update task')));
+  }
+
+  deleteTask(task: Task | number): Observable<Task> {
+    const id = typeof task === 'number' ? task : task.id;
+    const url = `${this.TASKS_URL}/${id}`;
+
+    return this.http
+      .delete<Task>(url, this.httpOptions)
+      .pipe(catchError(this.handleError<Task>()));
+  }
+
+  searchTasks(term: string): Observable<Task[]> {
+    if (!term.trim()) {
+      // if not search term, return empty task array.
+      return of([]);
+    }
+
+    return this.http
+      .get<Task[]>(`${this.TASKS_URL}/?name=${term}`)
+      .pipe(catchError(this.handleError<Task[]>([])));
+  }
+
+  private handleError<T>(result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
